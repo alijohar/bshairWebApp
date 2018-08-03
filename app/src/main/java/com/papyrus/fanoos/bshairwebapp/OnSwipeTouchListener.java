557bee53dfe72d23374ab0cbc1e61324c67498a3
ali johar <johar.ali1@gmail.com>
2018-08-03 23:44:11 +0430

    Add swipe back in news detail and delete Y listener

--- app/src/main/java/com/papyrus/fanoos/bshairwebapp/OnSwipeTouchListener.java
+++ app/src/main/java/com/papyrus/fanoos/bshairwebapp/OnSwipeTouchListener.java
@@ -23,10 +23,7 @@ public class OnSwipeTouchListener implements View.OnTouchListener {
         private static final int SWIPE_THRESHOLD = 100;
         private static final int SWIPE_VELOCITY_THRESHOLD = 100;
 
-        @Override
-        public boolean onDown(MotionEvent e) {
-            return true;
-        }
+
 
         @Override
         public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
@@ -44,14 +41,7 @@ public class OnSwipeTouchListener implements View.OnTouchListener {
                         result = true;
                     }
                 }
-                else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
-                    if (diffY > 0) {
-                        onSwipeBottom();
-                    } else {
-                        onSwipeTop();
-                    }
-                    result = true;
-                }
+
             } catch (Exception exception) {
                 exception.printStackTrace();
             }
@@ -65,9 +55,5 @@ public class OnSwipeTouchListener implements View.OnTouchListener {
     public void onSwipeLeft() {
     }
 
-    public void onSwipeTop() {
-    }
 
-    public void onSwipeBottom() {
-    }
 }
